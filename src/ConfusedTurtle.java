3fbe43c492b8ee0af0d3978d3ccbc97163528b86
sonikp <mfloerchinger@yahoo.com>
2016-10-25 08:35:47 -0700

    finished LNX

---
+++ src/ConfusedTurtle.java
@@ -0,0 +1,20 @@
+public class ConfusedTurtle extends Turtle
+{
+	///////////fields////////////////////////
+	
+	
+	////////////constructors/////////////////////
+
+	
+	////////////methods///////////////////////
+	
+	
+	
+	public static void main(String[] args)
+	{
+		System.out.println("ConfusedTurtle");
+		ConfusedTurtle ct = new ConfusedTurtle();
+		ct.
+		
+	}
+}
